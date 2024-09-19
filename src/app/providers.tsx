9cd22a64730535b805d2c3a55b58f33e9f16bf7e
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-09-19 14:04:04 -0300

    configurações iniciais

---
+++ src/app/providers.tsx
@@ -0,0 +1,24 @@
+import { ThemeProvider } from '@/components/ThemeProvider'
+import { Be_Vietnam_Pro } from 'next/font/google'
+import React from 'react'
+
+const BeVietnamProp = Be_Vietnam_Pro({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900", "100", "200", "300", "400", "500", "600", "700", "800", "900"]
+})
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <body className={`${BeVietnamProp.className} antialiased`}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </body>
+  )
+}
