8e02581832c16774177eae03e1386f93e38ca722
richGuilherme <richardbold.rg@gmail.com>
2024-09-30 23:59:31 -0300

    feat: add lib table + navigationBar + component  molecula table

--- src/components/root/DashBoard/Tickets/components/ScheduledActivities/index.tsx
+++ src/components/root/DashBoard/Tickets/components/ScheduledActivities/index.tsx
@@ -1,14 +1,23 @@
 import Container from "@/components/ui/organisms/container";
+import TableScheduleActivies from "./table";
+import { CalendarCheckIcon } from "lucide-react";
 
 const ScheduledActivities = () => {
+  const count = 2;
   return (
     <Container
       height="586px"
       width="100%"
       gradientColor="gradient_color-meio"
       title="Atividades agendadas"
+      subTitle={
+        <>
+          <CalendarCheckIcon size={17} className="text-green-400" />
+          <p>{count} atividades</p>
+        </>
+      }
     >
-      Teste
+      <TableScheduleActivies />
     </Container>
   );
 };
---
+++ src/components/root/DashBoard/Tickets/components/ScheduledActivities/table/index.tsx
@@ -0,0 +1,29 @@
+import { TableSimple } from "@/components/ui/molecules/tableSimple";
+import { columns, Payment } from "../columns";
+
+function getData(): Payment[] {
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 400,
+      status: "processing",
+      email: "m@example.com",
+    },
+  ];
+}
+
+export default function TableScheduleActivies() {
+  const data = getData();
+
+  return (
+    <div className="container mx-auto">
+      <TableSimple columns={columns} data={data} />
+    </div>
+  );
+}
