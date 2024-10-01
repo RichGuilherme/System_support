90a51c91a2f1061fc6d1806567616e59ead5eaed
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-10-01 12:40:42 -0300

    feat: component LastNotifications

--- src/components/root/DashBoard/Tickets/components/LastNotifications/index.tsx
+++ src/components/root/DashBoard/Tickets/components/LastNotifications/index.tsx
@@ -1,11 +1,30 @@
 import Container from "@/components/ui/organisms/container";
+import { TicketIcon } from "lucide-react";
 import React from "react";
 
 const LastNotifications = () => {
   return (
     <>
       <Container height="720px" width="100%" title="Últimas notificações">
-        teste
+        <div className="flex flex-row items-center justify-between">
+          <div className="flex items-center gap-3">
+            <TicketIcon />
+
+            <div>
+              <p className="text-base font-medium text-highlight-primary">
+                Novo pré-ticket
+              </p>
+              <p className="text-textSimples-300">
+                Fwd: ENC: Acesso ao servidor
+              </p>
+            </div>
+          </div>
+
+          <div className="flex gap-4">
+            <span>17/09/2024</span>
+            <span>09:58</span>
+          </div>
+        </div>
       </Container>
     </>
   );
