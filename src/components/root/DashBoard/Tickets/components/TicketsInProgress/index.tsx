eb3316605d8102b36cd148679c31aa26cca8384a
richGuilherme <richardbold.rg@gmail.com>
2024-10-01 08:01:58 -0300

    feat: ticketsInprogress

--- src/components/root/DashBoard/Tickets/components/TicketsInProgress/index.tsx
+++ src/components/root/DashBoard/Tickets/components/TicketsInProgress/index.tsx
@@ -1,16 +1,26 @@
 import Container from "@/components/ui/organisms/container";
+import NavMenu from "./NavMenu";
+import PieChart from "./PieChart";
+import { useState } from "react";
 
 const TicketsInProgress = () => {
+  const [data, setData] = useState({
+    responsaveis: [],
+    mesas: [],
+    clientes: [],
+    tiposDeRecursos: [],
+  });
+
   return (
     <>
       <Container
         width="100%"
         height="570px"
-        gridSpan="row-span-1"
         gradientColor="gradient_color-meio"
         title="Tickets em andamento"
       >
-        Teste
+        <NavMenu />
+        <PieChart />
       </Container>
     </>
   );
