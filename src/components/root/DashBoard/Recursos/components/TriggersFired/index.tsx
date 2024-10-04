0dde9f24f3e9ec9dcd36e551ae4b7da507cc2300
richGuilherme <richardbold.rg@gmail.com>
2024-10-04 12:33:18 -0300

    feat: ajuste no triggerFired

--- src/components/root/DashBoard/Recursos/components/TriggersFired/index.tsx
+++ src/components/root/DashBoard/Recursos/components/TriggersFired/index.tsx
@@ -1,9 +1,17 @@
 import Container from "@/components/ui/organisms/container";
 import TableTriggersFired from "./table";
+import { TooltipsIcon } from "@/components/ui/molecules/tooltipIcon";
 
 const TriggersFired = () => {
   return (
-    <Container height="586px" width="100%" title="Atividades agendadas">
+    <Container
+      height="586px"
+      width="100%"
+      title="Gatilhos disparados"
+      rightContent={
+        <TooltipsIcon content="Tabela contendo gatilhos disparados e não resolvidos ainda" />
+      }
+    >
       <TableTriggersFired />
     </Container>
   );
