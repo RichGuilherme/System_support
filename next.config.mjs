30865b7859dd03ae98bdc28fde6c0a029866f16e
richGuilherme <richardbold.rg@gmail.com>
2024-10-07 09:28:31 -0300

    feat: ajuste no redirecionamento de rotas

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,12 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/dashboard",
+        destination: "/dashboard/tickets",
+        permanent: true,
+      },
+      {
+        source: "/dashboard",
+        destination: "/dashboard/tickets",
         permanent: true,
       },
     ];
