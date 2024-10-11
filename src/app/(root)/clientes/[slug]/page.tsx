0b7f500c12f0910107acb3fb041b013d8d6b9f89
richGuilherme <richardbold.rg@gmail.com>
2024-10-11 13:10:05 -0300

    feat: compoents da rota dinâmica clientes

--- src/app/(root)/clientes/[slug]/page.tsx
+++ src/app/(root)/clientes/[slug]/page.tsx
@@ -1,7 +1,14 @@
+import { Framer } from "@/components/layout/Frame";
+import ClientsInfor from "@/components/root/Clientes/ClientsInfor";
 import React from "react";
 
 const ClientPageSlug = ({ params }: { params: { slug: string } }) => {
-  return <div>ClientPageSlug {decodeURIComponent(params.slug)}</div>;
+  const paramsValue = decodeURIComponent(params.slug);
+  return (
+    <Framer>
+      <ClientsInfor paramsValue={paramsValue} />
+    </Framer>
+  );
 };
 
 export default ClientPageSlug;
---
+++ src/components/root/Clientes/ClientsInfor/index.tsx
@@ -0,0 +1,40 @@
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/atoms/tabs";
+import Informations from "./informations";
+
+const ClientsInfor = ({ paramsValue }: { paramsValue: string }) => {
+  const name = "Richard Guilherme Afonso Silva";
+  const idUser = "086442413997";
+
+  return (
+    <Tabs defaultValue="informations" className="page-container">
+      <div className="mb-4">
+        <p className="text-2xl font-medium text-highlight-primary">
+          {paramsValue}
+        </p>
+
+        <div className="text-textSimples-300">{`${name} ${idUser}`}</div>
+      </div>
+
+      <TabsList>
+        <TabsTrigger value="informations">Informações</TabsTrigger>
+        <TabsTrigger value="users">Usuário</TabsTrigger>
+        <TabsTrigger value="tickets">Tickets</TabsTrigger>
+        <TabsTrigger value="contracts">Contratos</TabsTrigger>
+        <TabsTrigger value="resources">Recursos</TabsTrigger>
+        <TabsTrigger value="relationship">Relacionamentos</TabsTrigger>
+        <TabsTrigger value="billing">Faturamento</TabsTrigger>
+      </TabsList>
+
+      <TabsContent value="informations">
+        <Informations />
+      </TabsContent>
+    </Tabs>
+  );
+};
+
+export default ClientsInfor;
