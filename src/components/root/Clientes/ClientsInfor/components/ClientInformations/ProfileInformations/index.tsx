149bff7191bba70260fdaf23e2abce904a828224
richGuilherme <richardbold.rg@gmail.com>
2024-10-14 11:52:42 -0300

    feat: component profileInformations criado

--- src/components/root/Clientes/ClientsInfor/components/ClientInformations/ProfileInformations/index.tsx
+++ src/components/root/Clientes/ClientsInfor/components/ClientInformations/ProfileInformations/index.tsx
@@ -1,14 +1,116 @@
+import { Button } from "@/components/ui/atoms/button";
+import { Label } from "@/components/ui/atoms/label";
+import { Switch } from "@/components/ui/atoms/switch";
+import { Textarea } from "@/components/ui/atoms/textarea";
 import Container from "@/components/ui/organisms/container";
+import { Edit2Icon } from "lucide-react";
 import React from "react";
+import { PatternFormat } from "react-number-format";
 
 const ProfileInformations = () => {
-  return (
-    <Container width="100%">
-      <div className="flex h-[150px] w-[150px] items-center justify-center rounded-full border border-dashed bg-background text-sm">
-        + Logo
-      </div>
+  const razaoSocial = {
+    name: "Richard Guilherme Afonso Silva",
+    idUser: "086442413997",
+  };
 
-      <div></div>
+  const nomeFantasia = "Azapfy";
+  const inscricaoEstadual = "";
+  const inscricaoMunicial = "";
+  const pastaDeTrabalho = "";
+  const agentes = "";
+
+  return (
+    <Container
+      width="100%"
+      style="pt-5"
+      rightContent={
+        <>
+          <Button variant="ghost" className="flex gap-2">
+            Editar
+            <Edit2Icon size={23} />
+          </Button>
+        </>
+      }
+    >
+      <div className="flex flex-row gap-x-32">
+        <div className="space-y-6">
+          <div className="flex flex-col gap-y-2">
+            <Label>Nome fantasia:</Label>
+            {!!nomeFantasia ? <span>{nomeFantasia}</span> : <div>_ _</div>}
+          </div>
+
+          <div className="flex flex-col gap-y-2">
+            <Label>Inscrição estadual:</Label>
+            {!!inscricaoEstadual ? (
+              <span>{inscricaoEstadual}</span>
+            ) : (
+              <div>_ _</div>
+            )}
+          </div>
+
+          <div className="flex flex-col gap-y-2">
+            <Label>Pasta de trabalho:</Label>
+            {!!pastaDeTrabalho ? (
+              <span>{pastaDeTrabalho}</span>
+            ) : (
+              <div>_ _</div>
+            )}
+          </div>
+
+          <div className="flex flex-col gap-3 pt-8">
+            <Label htmlFor="billingSwitch">Faturamento quarterizado</Label>
+            <Switch id="billingSwitch" />
+          </div>
+        </div>
+
+        <div className="space-y-6">
+          <div className="flex flex-col gap-y-2">
+            <Label>Razão social:</Label>
+            {!!razaoSocial ? (
+              <span>{`${razaoSocial.name} ${razaoSocial.idUser}`}</span>
+            ) : (
+              <div>_ _</div>
+            )}
+          </div>
+
+          <div className="flex flex-col gap-y-2">
+            <Label>Inscrição municipal:</Label>
+            {!!inscricaoMunicial ? (
+              <span>{inscricaoMunicial}</span>
+            ) : (
+              <div>_ _</div>
+            )}
+          </div>
+
+          <div className="flex flex-col gap-3 pt-8">
+            <Label htmlFor="visibleClientSwitch">Visível para clientes:</Label>
+            <Switch id="visibleClientSwitch" />
+          </div>
+        </div>
+
+        <div className="space-y-6">
+          <div className="flex flex-col gap-y-2">
+            <Label>CPF/CNPJ:</Label>
+            <PatternFormat
+              format="
+                ##.###.###/####-##"
+              displayType="text"
+              allowEmptyFormatting
+              value={"15838266/0434-50"}
+            />
+          </div>
+
+          <div className="flex flex-col gap-y-2">
+            <Label>Agentes:</Label>
+            {!!agentes ? <span>{agentes}</span> : <div>_ _</div>}
+          </div>
+        </div>
+
+        <div className="flex w-1/4 flex-col gap-y-2">
+          <Label>Anotações:</Label>
+          <Textarea />
+        </div>
+      </div>
     </Container>
   );
 };
