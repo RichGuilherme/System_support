9f1de7d147bffd893c55aef748eb04c107835c79
richGuilherme <richardbold.rg@gmail.com>
2024-10-17 13:55:57 -0300

    feat: rota relatórios + component accondion

---
+++ src/app/(root)/relatorios/page.tsx
@@ -0,0 +1,12 @@
+import { Framer } from "@/components/layout/Frame";
+import Relatorios from "@/components/root/Relatorios";
+
+const ReportPage = () => {
+  return (
+    <Framer>
+      <Relatorios />
+    </Framer>
+  );
+};
+
+export default ReportPage;
