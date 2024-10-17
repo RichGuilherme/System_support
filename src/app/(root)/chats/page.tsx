16475d7f8b40a98d5671235856413015f7a6db36
richGuilherme <richardbold.rg@gmail.com>
2024-10-17 13:55:30 -0300

    feat: rota page chats

---
+++ src/app/(root)/chats/page.tsx
@@ -0,0 +1,8 @@
+import { Framer } from "@/components/layout/Frame";
+import React from "react";
+
+const ChatsIPage = () => {
+  return <Framer>ChatsIPage</Framer>;
+};
+
+export default ChatsIPage;
