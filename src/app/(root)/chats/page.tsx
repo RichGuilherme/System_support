bec64953dc3f13847a397407153c32920d00b379
richGuilherme <richardbold.rg@gmail.com>
2024-10-21 13:35:19 -0300

    feat: rota Chats + component

--- src/app/(root)/chats/page.tsx
+++ src/app/(root)/chats/page.tsx
@@ -1,8 +1,13 @@
 import { Framer } from "@/components/layout/Frame";
+import Chats from "@/components/root/Chats";
 import React from "react";
 
 const ChatsIPage = () => {
-  return <Framer>ChatsIPage</Framer>;
+  return (
+    <Framer>
+      <Chats />
+    </Framer>
+  );
 };
 
 export default ChatsIPage;
---
+++ src/components/root/Chats/index.tsx
@@ -0,0 +1,8 @@
+import Container from "@/components/ui/organisms/container";
+import React from "react";
+
+const Chats = () => {
+  return <Container width="100%" title="Chats"></Container>;
+};
+
+export default Chats;
