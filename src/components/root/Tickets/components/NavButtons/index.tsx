f361f9a9e7383d0279a5d10c156856956fe7eab0
richGuilherme <richardbold.rg@gmail.com>
2024-10-23 13:36:15 -0300

    ui: ajustes no design dos buttons

--- src/components/root/Tickets/components/NavButtons/index.tsx
+++ src/components/root/Tickets/components/NavButtons/index.tsx
@@ -11,17 +11,23 @@ const NavButtons = () => {
   return (
     <Container title="Tickets" width="100%" style="mb-4">
       <div className="flex w-full flex-row justify-start gap-x-4">
-        <Button variant="outline" className="gap-1 px-3 py-2">
-          <TicketIcon /> Pré-tickets
+        <Button variant="outline" className="gap-1 px-3 py-2 hover:border-red">
+          <TicketIcon className="text-red" /> Pré-tickets
         </Button>
-        <Button variant="outline" className="gap-1 px-3 py-2">
-          <ShieldCheckIcon /> Autorizações
+        <Button
+          variant="outline"
+          className="gap-1 px-3 py-2 hover:border-yellow"
+        >
+          <ShieldCheckIcon className="text-yellow" /> Autorizações
         </Button>
-        <Button variant="outline" className="gap-1 px-3 py-2">
-          <ListXIcon /> Tickets em revisão
+        <Button variant="outline" className="gap-1 px-3 py-2 hover:border-cyan">
+          <ListXIcon className="text-cyan" /> Tickets em revisão
         </Button>
-        <Button variant="outline" className="gap-1 px-3 py-2">
-          <TicketCheckIcon /> Tickets fechados
+        <Button
+          variant="outline"
+          className="gap-1 px-3 py-2 hover:border-green-500"
+        >
+          <TicketCheckIcon className="text-green-500" /> Tickets fechados
         </Button>
       </div>
     </Container>
